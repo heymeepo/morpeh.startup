49bbde91fa1e71774ff14f0630a602d35f0197d8
heymeepo <h3ystudio@gmail.com>
2024-01-21 01:51:18 +0400

    Add DefineSymbolHandler

---
+++ Runtime/DefineSymbolHandler.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Scellecs.Morpeh.Elysium
+{
+    [InitializeOnLoad]
+    public class DefineSymbolHandler
+    {
+        private const string SYMBOL = "MORPEH_ELYSIUM";
+
+        static DefineSymbolHandler()
+        {
+            if (IsDefineSymbolPresent(SYMBOL) == false)
+            {
+                AddDefineSymbol(SYMBOL);
+            }
+        }
+
+        private static bool IsDefineSymbolPresent(string define)
+        {
+            string existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            return existingDefines.Contains(define);
+        }
+
+        private static void AddDefineSymbol(string define)
+        {
+            string existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, existingDefines + ";" + define);
+        }
+    }
+}
+#endif
