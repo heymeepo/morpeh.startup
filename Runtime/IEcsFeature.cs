f4471c270d255771d37458c0e8d33ef778ed6e92
heymeepo <h3ystudio@gmail.com>
2024-05-03 17:51:32 +0400

    Remove unintentionally added defines from some files

--- Runtime/IEcsFeature.cs
+++ Runtime/IEcsFeature.cs
@@ -1,20 +1,7 @@
-﻿#if ENABLE_MONO || ENABLE_IL2CPP
-#define MORPEH_UNITY
-#endif
-
-#if VCONTAINER && !MORPEH_UNITY
-#undef VCONTAINER
-#endif
-
-#if VCONTAINER
-#endif
-
-using static Scellecs.Morpeh.Elysium.EcsStartup;
-
-namespace Scellecs.Morpeh.Elysium
+﻿namespace Scellecs.Morpeh.Elysium
 {
     public interface IEcsFeature
     {
-        public void Configure(FeatureBuilder builder);
+        public void Configure(EcsStartup.FeatureBuilder builder);
     }
 }
--- Runtime/IUpdateSystem.cs
+++ Runtime/IUpdateSystem.cs
@@ -1,15 +1,4 @@
-﻿#if ENABLE_MONO || ENABLE_IL2CPP
-#define MORPEH_UNITY
-#endif
-
-#if VCONTAINER && !MORPEH_UNITY
-#undef VCONTAINER
-#endif
-
-#if VCONTAINER
-#endif
-
-namespace Scellecs.Morpeh.Elysium
+﻿namespace Scellecs.Morpeh.Elysium
 {
     public interface IUpdateSystem : ISystem { }
 }
