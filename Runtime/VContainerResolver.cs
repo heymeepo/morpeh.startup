80a469998e681cf8ecdda2aa152cb4513dbeca53
heymeepo <h3ystudio@gmail.com>
2024-05-05 00:06:31 +0400

    Remove IContainerBuilder delegates usage on register, cache types instead and register them directly

--- Runtime/VContainerResolver.cs
+++ Runtime/VContainerResolver.cs
@@ -10,6 +10,7 @@
 using System;
 using VContainer.Unity;
 using VContainer;
+using System.Collections.Generic;
 
 namespace Scellecs.Morpeh.Elysium
 {
@@ -19,24 +20,29 @@ namespace Scellecs.Morpeh.Elysium
         private LifetimeScope featuresScope;
         private LifetimeScope systemsScope;
 
-        private Action<IContainerBuilder> registerFeatures;
-        private Action<IContainerBuilder> registerSystems;
+        private HashSet<Type> featuresTypes;
+        private HashSet<Type> systemsTypes;
 
-        public VContainerResolver(LifetimeScope currentScope) => scope = currentScope;
+        public VContainerResolver(LifetimeScope currentScope)
+        {
+            scope = currentScope;
+            featuresTypes = new HashSet<Type>(64);
+            systemsTypes = new HashSet<Type>(64);
+        }
 
-        public void BuildFeaturesContainer() => featuresScope = scope.CreateChild(builder => registerFeatures?.Invoke(builder));
+        public void BuildFeaturesContainer() => featuresScope = scope.CreateChild(builder => RegisterTypesInContainer(builder, featuresTypes));
 
-        public void BuildSystemsContainer() => systemsScope = scope.CreateChild(builder => registerSystems?.Invoke(builder));
+        public void BuildSystemsContainer() => systemsScope = scope.CreateChild(builder => RegisterTypesInContainer(builder, systemsTypes));
 
         public void Register(Type type, RegistrationDefinition definition)
         {
             if (definition == RegistrationDefinition.Feature)
             {
-                registerFeatures += (builder) => builder.Register(type, Lifetime.Transient);
+                featuresTypes.Add(type);
             }
             else
             {
-                registerSystems += (builder) => builder.Register(type, Lifetime.Transient);
+                systemsTypes.Add(type);
             }
         }
 
@@ -61,6 +67,14 @@ namespace Scellecs.Morpeh.Elysium
             if (systemsScope != null)
             {
                 systemsScope.Dispose();
+            }
+        }
+
+        private static void RegisterTypesInContainer(IContainerBuilder builder, HashSet<Type> types)
+        { 
+            foreach (var type in types) 
+            {
+                builder.Register(type, Lifetime.Transient);
             }
         }
     }
